dbe4b02ee234a0af214d2e7a8ab629fd7d80311d
ostapkonst <ostap.konstantinov@yandex.ru>
2016-08-13 17:42:55 +1000

    Change RUN.bat file

---
+++ TestingApp/RUN (All tests).bat
@@ -0,0 +1,4 @@
+@echo off
+:: Запуск всех тестов из папки Input для программы TextToASCII для size = 10
+TestingApp.exe TextToASCII.exe 10
+pause
--- TestingApp/Run ests.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: Запуск всех тестов из папки Input для программы TextToASCII для size = 10
-TestingApp.exe TextToASCII.exe 10
-pause
